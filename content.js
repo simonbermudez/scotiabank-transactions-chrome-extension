5e22474fa152460457083cee9c5a763610cd981d
Simon Bermudez <simon.bermudez@oncallhealth.com>
2024-09-09 12:34:44 -0300

    alert success transactions

--- content.js
+++ content.js
@@ -1,54 +1,11 @@
-function parseDate(dateStr) {
-    // Create a new Date object from the input date string
-    const date = new Date(dateStr);
-
-    // Get the year, month, and day from the Date object
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based, so add 1
-    const day = String(date.getDate()).padStart(2, '0'); // Ensure day is two digits
-
-    // Format the date as YYYY-MM-DD
-    const formattedDate = `${year}-${month}-${day}`;
-    return formattedDate;
+function parseDate(dateString) {
+    // Parse date from string format "YYYY-MM-DDTHH:MM:SS" to "YYYY-MM-DD"
+    // TODO: Backend should be able to handle the original format (change date to datetime)
+    return dateString.split("T")[0];
 }
 
-const processTransactionsNodeList = (transactionsNodeList) => {
-    if (!transactionsNodeList) {
-        return []
-    }
-    const transactions = Array.from(transactionsNodeList.querySelectorAll("tr:not([class])")).flatMap(transaction => {
-        console.log("processing transaction")
-        try {
-            amount = transaction.querySelectorAll("td")[2].innerText ? - Number(transaction.querySelectorAll("td")[2].innerText) : Number(transaction.querySelectorAll("td")[3].innerText)
-            return {
-                date: parseDate(transaction.querySelector("th").innerText),
-                description: transaction.querySelectorAll("td")[1].innerText.split("\n")[0],
-                amount: amount ? amount : 0,
-            }
-        } catch (e) {
-            console.log("error processing transaction")
-            return []
-        }
-    })
-    console.log(transactions)
-    return transactions
-}
 
-const getTransactions = () => {
-    console.log("Loading Transactions...")
-    const accountName = document.querySelector(".regular-data-account-details > h3").innerText
-    const [pendingTransactions, postedTransactions] = document.querySelectorAll(".thtable")
-    return {accountName, transactions: processTransactionsNodeList(pendingTransactions).concat(
-        processTransactionsNodeList(postedTransactions)
-    ).map(transaction => {
-        return {
-            id: `${accountName}-${transaction.date}-${transaction.description}-${transaction.amount}`.replaceAll(" ", "-"),
-            ...transaction,
-        }})
-    }
-}
-
-async function uploadToServer(data = {}) {
+async function uploadToTransaktor(transactions = {}) {
     url = "https://transaktor.bermudez.ca/transactions/upload/"
     // Default options are marked with *
     const response = await fetch(url, {
@@ -62,29 +19,54 @@ async function uploadToServer(data = {}) {
       },
       redirect: "follow", // manual, *follow, error
       referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify(data), // body data type must match "Content-Type" header
+      body: JSON.stringify(transactions), // body data type must match "Content-Type" header
     });
-    console.log(response.json()); // parses JSON response into native JavaScript objects
-  }
 
-const scrapeTransactions = async () => {
-    console.log("Scraping Transactions")
-    chrome.runtime.sendMessage({gettingTransactions: true}, (response) => {
-        console.log(response)
-    })
-    return getTransactions()
-}
-window.onload = async (event) => {
-    console.log('page is fully loaded');
-    scrapeTransactions().then(transactions => {
-        console.log(transactions)
-        chrome.storage.local.set({ transactions: transactions }, function(){
-            console.log('Value is set to ', transactions);
-        });
-        console.log("uploading to server");
-        // post request to server to save transactions
-        uploadToServer(transactions.transactions);
+    let data = await response.json()
+
+    if (data.created) {
+        alert(`${data.created.length} Transactions uploaded successfully to Transaktor`)
     }
-    )
-};
+    console.log(data); 
+}
+
+async function getDataFromScotiaBank() {
+    // get path without querystring
+    var path = window.location.pathname;
+
+    // remove /acccounts from path
+    path = path.replace('/accounts/', '');
+
+    
+    let response = await fetch(`https://secure.scotiabank.com/api/${path}/transactions?limit=300`)
+    data =  await response.json()
+    data = data.data
+
+    let mapper = t => {
+        return {
+            id: t.id,
+            date: parseDate(t.transactionDate),
+            description: t.description,
+            amount: t.transactionType == "DEBIT" ? - t.transactionAmount.amount : t.transactionAmount.amount,
+        }
+    }
+
+    // Process pending transactions 
+    let pendingTransactions = data.pending.map(mapper)
+
+    // Process pending transactions 
+    let settledTransactions = data.settled.map(mapper)
+
+    // Combine both transactions
+    let transactions = pendingTransactions.concat(settledTransactions)
+
+    uploadToTransaktor(transactions)
+}
+
+let url = window.location.href
+
+
+if (url.includes('scotiabank')) {
+    getDataFromScotiaBank();
+}
 
